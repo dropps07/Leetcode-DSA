b63fea3d363ee0f765cfb5766cc3b433f7186ff6
Ajey Awasthi <115537579+dropps07@users.noreply.github.com>
2025-01-03 21:53:25 +0530

    Update 189- RotateArray.java
    fix error

--- Data, Structures and Algorithms/189- RotateArray.java
+++ Data, Structures and Algorithms/189- RotateArray.java
@@ -2,12 +2,12 @@ class solution{
 public void rotate(int k, int [] nums){
 
 int temp[] = new int[nums.length];
-int k = k% nums.length;
+k = k% nums.length;
 for(int i =0;i<nums.length;i++){
 temp[(k+i)%nums.length]= nums[i];
 }
 for(int i =0;i< nums.length;i++){
-nums[i] = temo[i];
+nums[i] = temp[i];
 }
 }
 }
